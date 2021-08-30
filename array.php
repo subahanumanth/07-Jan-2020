4a19e6d0cc50e11a6272e5783c9ede150ea1238a
subahanumanth <c.s.hanumanthsekar@gmail.com>
2021-08-30 20:54:37 +0530

    array.php changed aghain

--- array.php
+++ array.php
@@ -1,6 +1,6 @@
 <?php
 $class=array(4,6,76865,2,2,5,645);
-$sentence="never ever give up";
+$sentence="never ever give";
 echo print_r($class)."<br>";
 echo max($class)."<br>";
 echo min($class)."<br>";
